a500da8a13b527017f1f535ab1d3839d3dbe00b8
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-03-23 17:10:13 +0100

    fix(dae)
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

--- DataModule/DAE.py
+++ DataModule/DAE.py
@@ -23,18 +23,18 @@ class DAEDataSet(DataSet):
         return 5
 
     def __str__(self) -> str:
-        if self.distance is not None:
-            self.distance = random.uniform(0, 30)
-        self.distance = round(random.uniform(0, 30), 2)
+        if self.self.distance is not None:
+            self.self.distance = random.uniform(0, 30)
+        self.self.distance = round(random.uniform(0, 30), 2)
         return random.choice([
-            "Une DAE est proche, à une distance d'environ " + str(distance) + " kilomètres.",
-            "À proximité, vous trouverez une DAE, située à environ " + str(distance) + " kilomètres.",
-            "Il y a une DAE dans les environs, à une distance d'environ " + str(distance) + " kilomètres.",
-            "Vous pouvez trouver une DAE à proximité, à environ " + str(distance) + " kilomètres.",
-            "Une DAE est présente à proximité, à environ " + str(distance) + " kilomètres.",
-            "À cet endroit, une DAE est disponible à une distance d'environ " + str(distance) + " kilomètres.",
-            "À proximité, vous découvrirez une DAE à une distance d'environ " + str(distance) + " kilomètres.",
-            "Une DAE est établie dans les environs, à environ " + str(distance) + " kilomètres.",
-            "Il y a une DAE non loin d'ici, à environ " + str(distance) + " kilomètres.",
-            "À quelques pas d'ici, vous trouverez une DAE à une distance d'environ " + str(distance) + " kilomètres."
+            "Une DAE est proche, à une self.distance d'environ " + str(self.distance) + " kilomètres.",
+            "À proximité, vous trouverez une DAE, située à environ " + str(self.distance) + " kilomètres.",
+            "Il y a une DAE dans les environs, à une self.distance d'environ " + str(self.distance) + " kilomètres.",
+            "Vous pouvez trouver une DAE à proximité, à environ " + str(self.distance) + " kilomètres.",
+            "Une DAE est présente à proximité, à environ " + str(self.distance) + " kilomètres.",
+            "À cet endroit, une DAE est disponible à une self.distance d'environ " + str(self.distance) + " kilomètres.",
+            "À proximité, vous découvrirez une DAE à une self.distance d'environ " + str(self.distance) + " kilomètres.",
+            "Une DAE est établie dans les environs, à environ " + str(self.distance) + " kilomètres.",
+            "Il y a une DAE non loin d'ici, à environ " + str(self.distance) + " kilomètres.",
+            "À quelques pas d'ici, vous trouverez une DAE à une self.distance d'environ " + str(self.distance) + " kilomètres."
         ])
