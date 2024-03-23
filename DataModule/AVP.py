5b820fededacbb233081a5a0b4a713ab98ef59f0
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-03-23 15:42:39 +0100

    update(starhouse, avp, hair):add random for str
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

--- DataModule/AVP.py
+++ DataModule/AVP.py
@@ -13,11 +13,22 @@ class AVP(AData):
         return self.__dataSetType
 
 class AVPDataSet(DataSet):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, distance=None, **kwargs) -> None:
         super().__init__(**kwargs)
+        self.distance = distance
+        if self.distance is None:
+            self.distance = round(random.uniform(0, 30), 2)
 
     def __str__(self) -> str:
-        if not self.__dict__.get("distance"):
-            self.distance = random.uniform(0, 30)
-        self.distance = round(self.distance, 2)
-        return "Il y a un accident corporel sur la voie publique à moins de " + str(self.distance) + "Km d'ici."
+        return random.choice([
+            "À moins de " + str(self.distance) + " kilomètres d'ici, un accident impliquant des blessés s'est produit sur la voie publique.",
+            "Près d'ici, à une distance d'environ " + str(self.distance) + " kilomètres, un accident corporel a eu lieu sur la voie publique.",
+            "Sur la voie publique à moins de " + str(self.distance) + " kilomètres d'ici, il y a eu un accident impliquant des blessés.",
+            "À moins de " + str(self.distance) + " kilomètres d'ici, un accident impliquant des blessés s'est produit sur la route.",
+            "Un accident corporel s'est produit sur la voie publique, à une distance d'environ " + str(self.distance) + " kilomètres d'ici.",
+            "À moins de " + str(self.distance) + " kilomètres d'ici, un incident avec des blessés a eu lieu sur la voie publique.",
+            "Il y a eu un accident impliquant des blessés sur la route publique, à environ " + str(self.distance) + " kilomètres d'ici.",
+            "À " + str(self.distance) + " kilomètres d'ici, un accident corporel s'est produit sur la voie publique.",
+            "Près d'ici, à moins de " + str(self.distance) + " kilomètres, un accident avec des blessés s'est produit sur la route.",
+            "Sur la voie publique, à une distance d'environ " + str(self.distance) + " kilomètres d'ici, un accident corporel a été signalé."
+        ])
--- DataModule/StarHouse.py
+++ DataModule/StarHouse.py
@@ -1,6 +1,6 @@
 from .AData import AData
 from .DataSet import DataSet
-import math
+import random
 
 class StarHouse(AData):
     def __init__(self, filePath: str) -> None:
@@ -17,6 +17,18 @@ class StarHouse(AData):
 class StarHouseDataSet(DataSet):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
+        self.__star_house_info = [
+            "Cet endroit était le lieu de résidence de l'éminent " + self.__dict__.get('prompt') + ".",
+            "Ici, a vécu l'illustre " + self.__dict__.get('prompt') + ".",
+            "C'est ici que l'illustre " + self.__dict__.get('prompt') + " avait élu domicile.",
+            "L'éminent " + self.__dict__.get('prompt') + " a résidé en ce lieu.",
+            "À cet endroit, nous retrouvons la demeure de l'illustre " + self.__dict__.get('prompt') + ".",
+            "L'illustre " + self.__dict__.get('prompt') + " avait choisi de vivre ici.",
+            "C'est ici que résidait l'éminent " + self.__dict__.get('prompt') + ".",
+            "En ce lieu, a vécu l'illustre " + self.__dict__.get('prompt') + ".",
+            "Cet endroit a été la résidence de l'éminent " + self.__dict__.get('prompt') + ".",
+            "Ici résidait l'illustre " + self.__dict__.get('prompt') + "."
+        ]
 
     def __str__(self) -> str:
-        return "L'illustre " + self.__dict__.get('prompt') + " a vécu ici."
+        return random.choice(self.__star_house_info)
