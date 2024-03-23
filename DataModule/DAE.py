5872f565265ae3c9a53acf3769860c4a04b52b20
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-03-23 17:01:46 +0100

    fix(spead) && upt(dae)
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

--- DataModule/DAE.py
+++ DataModule/DAE.py
@@ -1,24 +1,11 @@
-from .AData import AData
-from .DataSet import DataSet
 import random
-
-class DAE(AData):
-    def __init__(self, filePath: str) -> None:
-        super().__init__(filePath)
-        self.__dataSetType: DataSet = DAEDataSet
-
-    @property
-    def dataSetType(self) -> DataSet:
-        return self.__dataSetType
-
-    def abs(self) -> None:
-        pass
 
 class DAEDataSet(DataSet):
     def __init__(self, distance=None, **kwargs) -> None:
         super().__init__(**kwargs)
-        self.distance = distance
-        if self.distance is None:
+        if distance is not None:
+            self.distance = distance
+        else:
             self.distance = round(random.uniform(0, 30), 2)
 
     @staticmethod
@@ -26,18 +13,16 @@ class DAEDataSet(DataSet):
         return 5
 
     def __str__(self) -> str:
-        if self.distance is not None:
-            self.distance = random.uniform(0, 30)
-        self.distance = round(random.uniform(0, 30), 2)
+        distance = round(random.uniform(0, 30), 2)
         return random.choice([
-            "Une installation Seveso est proche, à une distance d'environ " + str(self.distance) + " kilomètres.",
-            "À proximité, vous trouverez une installation Seveso, située à environ " + str(self.distance) + " kilomètres.",
-            "Il y a une installation Seveso dans les environs, à une distance d'environ " + str(self.distance) + " kilomètres.",
-            "Vous pouvez trouver une installation Seveso à proximité, à environ " + str(self.distance) + " kilomètres.",
-            "Une installation Seveso est présente à proximité, à environ " + str(self.distance) + " kilomètres.",
-            "À cet endroit, une installation Seveso est disponible à une distance d'environ " + str(self.distance) + " kilomètres.",
-            "À proximité, vous découvrirez une installation Seveso à une distance d'environ " + str(self.distance) + " kilomètres.",
-            "Une installation Seveso est établie dans les environs, à environ " + str(self.distance) + " kilomètres.",
-            "Il y a une installation Seveso non loin d'ici, à environ " + str(self.distance) + " kilomètres.",
-            "À quelques pas d'ici, vous trouverez une installation Seveso à une distance d'environ " + str(self.distance) + " kilomètres."
+            "Une DAE est proche, à une distance d'environ " + str(distance) + " kilomètres.",
+            "À proximité, vous trouverez une DAE, située à environ " + str(distance) + " kilomètres.",
+            "Il y a une DAE dans les environs, à une distance d'environ " + str(distance) + " kilomètres.",
+            "Vous pouvez trouver une DAE à proximité, à environ " + str(distance) + " kilomètres.",
+            "Une DAE est présente à proximité, à environ " + str(distance) + " kilomètres.",
+            "À cet endroit, une DAE est disponible à une distance d'environ " + str(distance) + " kilomètres.",
+            "À proximité, vous découvrirez une DAE à une distance d'environ " + str(distance) + " kilomètres.",
+            "Une DAE est établie dans les environs, à environ " + str(distance) + " kilomètres.",
+            "Il y a une DAE non loin d'ici, à environ " + str(distance) + " kilomètres.",
+            "À quelques pas d'ici, vous trouverez une DAE à une distance d'environ " + str(distance) + " kilomètres."
         ])
--- DataModule/Speed.py
+++ DataModule/Speed.py
@@ -27,14 +27,14 @@ class SpeedDataSet(DataSet):
 
     def __str__(self) -> str:
         return random.choice([
-            "Il y a eu un excès de vitesse à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.datre + ".",
-            "Un excès de vitesse a été enregistré à proximité, à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.datre + ".",
-            "Sur la route, à environ " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été constaté le " + self.datre + ".",
-            "À moins de " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été observé le " + self.datre + ".",
-            "Le " + self.datre + ", un excès de vitesse a été relevé à une distance d'environ " + str(self.distance) + " kilomètres d'ici.",
-            "Un excès de vitesse a été détecté sur la route, à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.datre + ".",
-            "À proximité, le " + self.datre + ", un excès de vitesse a été enregistré à environ " + str(self.distance) + " kilomètres d'ici.",
-            "Un excès de vitesse a été constaté à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.datre + ".",
-            "Le " + self.datre + ", sur la route à proximité, un excès de vitesse a été repéré à environ " + str(self.distance) + " kilomètres d'ici.",
-            "À environ " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été signalé le " + self.datre + "."
+            "Il y a eu un excès de vitesse à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.date + ".",
+            "Un excès de vitesse a été enregistré à proximité, à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.date + ".",
+            "Sur la route, à environ " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été constaté le " + self.date + ".",
+            "À moins de " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été observé le " + self.date + ".",
+            "Le " + self.date + ", un excès de vitesse a été relevé à une distance d'environ " + str(self.distance) + " kilomètres d'ici.",
+            "Un excès de vitesse a été détecté sur la route, à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.date + ".",
+            "À proximité, le " + self.date + ", un excès de vitesse a été enregistré à environ " + str(self.distance) + " kilomètres d'ici.",
+            "Un excès de vitesse a été constaté à une distance d'environ " + str(self.distance) + " kilomètres d'ici, le " + self.date + ".",
+            "Le " + self.date + ", sur la route à proximité, un excès de vitesse a été repéré à environ " + str(self.distance) + " kilomètres d'ici.",
+            "À environ " + str(self.distance) + " kilomètres d'ici, un excès de vitesse a été signalé le " + self.date + "."
         ])
